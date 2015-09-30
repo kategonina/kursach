66f295c074c2b4ce1e0a3a582e876a82683951e5
kategonina <gloriya.95@mail.ru>
2015-09-30 21:36:04 +0300

    Практика

--- ruby/pr4.rb
+++
@@ -1,6 +0,0 @@
-
-nums = [3,5,6,8,10,4,4,46,4,5]  
-first, last = nums.slice(0), nums.slice(-1)
-s_elem = nums.find_all {|n|  n > first && n < last}.inject("[ ]") {|r, n|  r = n}
-s_elem_index = nums.rindex s_elem
-puts "#{s_elem_index} => #{s_elem}"
